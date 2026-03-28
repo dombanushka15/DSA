17392b2874c319dec11b16327d9c2a5b229cb02a
dombanushka15 <dombanushka@gmail.com>
2026-03-28 16:44:06 +0530

    add new ones

--- _03_STL/_06_iterators.cpp
+++ _03_STL/_06_iterators.cpp
@@ -4,8 +4,8 @@ using namespace std;
 int main() {
     vector<pair<int, string>> v;
     v.push_back({90, "Anushka"});
-    v.push_back({95, "payal"});
-    v.push_back({90, "sanika"});
+    v.push_back({95, "Avanti"});
+    v.push_back({90, "Sarthak"});
     sort(v.begin(), v.end());
     vector<pair<int, string>>::iterator it;
     cout << "Sorted list:\n";
